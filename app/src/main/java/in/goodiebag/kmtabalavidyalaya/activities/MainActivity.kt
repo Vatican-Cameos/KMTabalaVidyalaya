df313b8a79f1babd2a102d4905501af63f038cd2
Pavan <pavanvenkatesh94@gmail.com>
2017-05-29 11:06:06 +0530

    :sparkles: raw added

--- app/src/main/java/in/goodiebag/kmtabalavidyalaya/activities/MainActivity.kt
+++ app/src/main/java/in/goodiebag/kmtabalavidyalaya/activities/MainActivity.kt
@@ -1,16 +1,14 @@
-package `in`.goodiebag.kmtabalavidyalaya
+package `in`.goodiebag.kmtabalavidyalaya.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+
+import `in`.goodiebag.kmtabalavidyalaya.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ripple.startRippleAnimation();
-
-
-
     }
 }
---
+++ app/src/main/java/in/goodiebag/kmtabalavidyalaya/activities/SplashActivity.kt
@@ -0,0 +1,23 @@
+package `in`.goodiebag.kmtabalavidyalaya.activities
+
+import `in`.goodiebag.kmtabalavidyalaya.R
+import android.media.MediaPlayer
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.splash_activity.*
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var mediaPlayer: MediaPlayer;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+        ripple.startRippleAnimation()
+        mediaPlayer = MediaPlayer.create(this, R.raw.ringtaal);
+        mediaPlayer.start()
+        ripple.setOnClickListener {
+            mediaPlayer.stop();
+            ripple.stopRippleAnimation();
+        }
+    }
+
+}
